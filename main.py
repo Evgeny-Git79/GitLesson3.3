cac9133a24f6ac4136f3cdb3a5851928812b8cc0
Evgeny-Git79 <evgeny.zabolotnikov@gmail.com>
2024-12-09 12:30:19 +1000

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.examples.go_over_there import screen, running
 
 pygame.init()
@@ -7,7 +8,18 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load()
+icon = pygame.image.load("img/png-transparent-infographic-shooting-target.png")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/target.png")
+target_width = 80
+target_height = 80
+
+target_x = random.randint(0,SCREEN_WIDTH-target_width)
+target_y = random.randint(0, SCREEN_HEIGHT-target_height)
+
+
+
 while running:
     pass
 
